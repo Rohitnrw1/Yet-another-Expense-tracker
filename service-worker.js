98f6a88081f60804fd0305c82ac2a1768d6fd005
Rohitnrw1 <83217178+Rohitnrw1@users.noreply.github.com>
2025-10-24 15:41:15 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'finance-tracker-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html', 
+  '/bundle.js', // Include your main JS bundle
+  '/manifest.json'
+  // Add paths to other critical assets like CSS files and icons
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // Fallback to network
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Delete old caches
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
